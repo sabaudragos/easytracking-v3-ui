2d1cab4461dd3aebceab74cd7bafef35c1ac3b44
Dragos <sabau.dragos@gmail.com>
2020-01-26 22:55:38 +0000

    Test 9

--- src/app/sprint/sprint.component.ts
+++ src/app/sprint/sprint.component.ts
@@ -88,7 +88,7 @@ export class SprintComponent implements OnInit {
   }
 
   addNewTask() {
-    console.log("Testing the build - 5");
+    console.log("Testing the build - 6");
     let boardItemForm: FormGroup = this.formBuilder.group({
       'id': new FormControl(null),
       'title': new FormControl("", Validators.required),
