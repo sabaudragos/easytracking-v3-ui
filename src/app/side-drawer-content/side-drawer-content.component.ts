b2b335de5149afb47f7fabc2de15a0525a2698c2
Dragos <sabau.dragos@gmail.com>
2020-02-01 07:45:23 +0000

    Fixed user user admin path

--- src/app/side-drawer-content/side-drawer-content.component.ts
+++ src/app/side-drawer-content/side-drawer-content.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ToastrService} from "ngx-toastr";
 import {AuthService} from "../service/auth-service";
 import {Router} from "@angular/router";
+import {SideNavService} from "../service/side-nav-service";
 
 @Component({
   selector: 'app-side-drawer-content',
@@ -11,6 +12,7 @@ import {Router} from "@angular/router";
 export class SideDrawerContentComponent implements OnInit {
 
   constructor(private authService: AuthService,
+              private sideNavService: SideNavService,
               private router: Router,
               private toastr: ToastrService) {
   }
@@ -26,11 +28,11 @@ export class SideDrawerContentComponent implements OnInit {
   }
 
   onProfileClick() {
+    this.sideNavService.announceSideNavOn();
     this.router.navigateByUrl("/user")
   }
 
   isAuthenticated(): boolean {
     return this.authService.isAuthenticated();
   }
-
 }
